cfb84fc79401e94e8c20924e17cd6a0c37a31ad7
dtt4h <dtt4h322@gmail.com>
2025-10-02 22:31:40 +0700

    feat:implement struct for foreign API

--- models/types.go
+++ models/types.go
@@ -11,3 +11,8 @@ type ConvertRequest struct {
 type ConvertResponse struct {
 	Result float64 `json:"result"`
 }
+
+type ExchangeRates struct {
+	Rates map[string]float64 `json:"rates"`
+	Base  string             `json:"base"`
+}
